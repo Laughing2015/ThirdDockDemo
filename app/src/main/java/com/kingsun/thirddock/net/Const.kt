56a67a7db9d4f4a3d31ced73edc7f8e0f4e92207
liuhangya <3171898896@qq.com>
2022-08-15 13:56:15 +0800

    更新环境为正式环境，更新APK包

--- app/src/main/java/com/kingsun/thirddock/net/Const.kt
+++ app/src/main/java/com/kingsun/thirddock/net/Const.kt
@@ -12,7 +12,8 @@ object Const {
     //合作方秘钥
     const val SECRET_KEY = "089f79cffa8af74e1966ba2fa34cd370"
 
-    private const val BASE_URL = "http://183.47.42.218:9222/"
+//    private const val BASE_URL = "http://183.47.42.218:9222/"
+    private const val BASE_URL = "https://tbxapi.kingsun.cn/"
     //api地址
     const val API_URL = "${BASE_URL}aop/active"
     /*获取书本资源信息*/
